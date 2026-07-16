7e3bc12c70a5a626ae215fd95887a43f51f3329f
pooya-pc <ap.poorasa@gmail.com>
2026-07-16 11:00:41 +0330

    availability: filter villa search by check_in/check_out range

--- villas/views.py
+++ villas/views.py
@@ -9,6 +9,9 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from rest_framework.generics import ListCreateAPIView
 
+from django.db.models import Count, Q
+from django.utils.dateparse import parse_date
+
 class VillaListCreateAPIView(ListCreateAPIView):
 
     serializer_class = VillaSerializer
@@ -21,6 +24,24 @@ class VillaListCreateAPIView(ListCreateAPIView):
         if city:
             queryset = queryset.filter(city__iexact=city)
 
+        check_in = self.request.query_params.get("check_in")
+        check_out = self.request.query_params.get("check_out")
+        if check_in and check_out:
+            check_in_date = parse_date(check_in)
+            check_out_date = parse_date(check_out)
+            if check_in_date and check_out_date and check_in_date < check_out_date:
+                nights_requested = (check_out_date - check_in_date).days
+
+                queryset = queryset.annotate(
+                    free_nights=Count(
+                        "availabilities",
+                        filter=Q(
+                            availabilities__date__gte=check_in_date,
+                            availabilities__date__lt=check_out_date,
+                            availabilities__is_available=True,
+                        ),
+                    )
+                ).filter(free_nights=nights_requested)
         return queryset
 
     def get_permissions(self):
